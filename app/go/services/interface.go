0559a3f17bb2ed4be5ea018db2b290fa26527bac
harsh <66548698+harsssh@users.noreply.github.com>
2023-11-06 18:46:22 +0900

    use go-run

--- app/go/services/interface.go
+++ app/go/services/interface.go
@@ -2,14 +2,14 @@ package services
 
 import "problem1/models"
 
-//go:generate moq -pkg repository -skip-ensure -out ../repository/mock.go . IUserRepository
+//go:generate go run github.com/matryer/moq -pkg repository -skip-ensure -out ../repository/mock.go . IUserRepository
 type IUserRepository interface {
 	FindFriendsByUserID(userID int) ([]*models.User, error)
 	FindFriendsOfFriendsByUserID(userID int) ([]*models.User, error)
 	FindFriendsOfFriendsPagingByUserID(userID int, page int, limit int) ([]*models.User, error)
 }
 
-//go:generate moq -out ./mock.go . IUserService
+//go:generate go run github.com/matryer/moq -out ./mock.go . IUserService
 type IUserService interface {
 	GetFriendList(userID int) ([]*models.User, error)
 	GetFriendOfFriendList(userID int) ([]*models.User, error)

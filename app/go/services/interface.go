aae7bdb0f71b5e3e5b0fa1dce7ce7170cbbb23fa
harsh <66548698+harsssh@users.noreply.github.com>
2023-11-06 16:09:24 +0900

    use moq

--- app/go/services/interface.go
+++ app/go/services/interface.go
@@ -2,20 +2,16 @@ package services
 
 import "problem1/models"
 
+//go:generate moq -pkg mocks -out ../mocks/user_repository.go . IUserRepository
 type IUserRepository interface {
 	FindFriendsByUserID(userID int) ([]*models.User, error)
 	FindFriendsOfFriendsByUserID(userID int) ([]*models.User, error)
 	FindFriendsOfFriendsPagingByUserID(userID int, page int, limit int) ([]*models.User, error)
 }
 
+//go:generate moq -pkg mocks -out ../mocks/user_service.go . IUserService
 type IUserService interface {
 	GetFriendsByUserID(userID int) ([]*models.User, error)
 	GetFriendsOfFriendsByUserID(userID int) ([]*models.User, error)
 	GetFriendsOfFriendsPagingByUserID(userID int, page int, limit int) ([]*models.User, error)
-}
-
-//go:generate bulkmockgen -use_go_run ISet -- -package mocks -destination ../mocks/mock.go
-var ISet = []any{
-	new(IUserRepository),
-	new(IUserService),
 }

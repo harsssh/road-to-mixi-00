a75b0244964e91f2dbfd73625301f290aee32f75
harsh <66548698+harsssh@users.noreply.github.com>
2023-11-08 00:41:19 +0900

    add dummy data

--- mysql/sql/1_dummy.sql
+++ mysql/sql/1_dummy.sql
@@ -1,8 +1,8 @@
 INSERT INTO `users` (`user_id`, `name`) VALUES
-(1, 'user1'), (2, 'user2'), (3, 'user3'), (4, 'user4'), (5, 'user5');
+(1, 'user1'), (2, 'user2'), (3, 'user3'), (4, 'user4'), (5, 'user5'), (6, 'user6'), (7, 'user7');
 
 INSERT INTO `friend_link` (`user1_id`, `user2_id`) VALUES
-(1, 2), (1, 3), (2, 3), (2, 4), (2, 5);
+(1, 2), (1, 3), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7);
 
 INSERT INTO `block_list` (`user1_id`, `user2_id`)  VALUES
 (1, 5);

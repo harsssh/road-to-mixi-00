c0d4a21fbaa31ec0640cc2aa09d1570e3a269aaf
harsh <66548698+harsssh@users.noreply.github.com>
2023-11-09 17:53:59 +0900

    update dummy data

--- mysql/sql/1_dummy.sql
+++ mysql/sql/1_dummy.sql
@@ -1,5 +1,5 @@
 INSERT INTO `users` (`user_id`, `name`) VALUES
-(1, 'user1'), (2, 'user2'), (3, 'user3'), (4, 'user4'), (5, 'user5'), (6, 'user6'), (7, 'user7');
+(10, 'user1'), (20, 'user2'), (30, 'user3'), (40, 'user4'), (50, 'user5'), (60, 'user6'), (70, 'user7');
 
 INSERT INTO `friend_link` (`user1_id`, `user2_id`) VALUES
 (1, 2), (1, 3), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7);
